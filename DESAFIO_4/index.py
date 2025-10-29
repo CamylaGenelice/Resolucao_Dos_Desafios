4dc99baac409a089f72986431eff5a1f71f82755
Camyla Genelice <genelicecamyla@gmail.com>
2025-10-29 18:28:05 -0300

    feat: script de leitura e escrita
    leitura e escrita em arquivos de texto

---
+++ DESAFIO_4/index.py
@@ -0,0 +1,40 @@
+'''
+file = open('teste.txt', 'w')
+
+file.write('Hello, World!')
+ler = open('teste.txt', 'r')
+conteudo = ler.read()
+print(conteudo)
+file.close()
+'''
+import sys
+import os 
+
+sys.path.append('../documentos')
+
+
+def leitura_arquivo(url):
+    try:
+        file = open(url, 'r', encoding='utf-8')
+        conteudo = file.read()
+        file.close()
+        return conteudo
+    except FileNotFoundError:
+        print("Arquivo não encontrado.")
+        return None    
+ 
+def escrita_arquivo(url, conteudo):
+    try:
+        file = open(url, 'w', encoding='utf-8')
+        file.write(conteudo)
+        file.close()
+        
+    except FileNotFoundError:
+        print("Arquivo não encontrado.")
+        
+    
+    
+    
+conteudo = escrita_arquivo('DESAFIO_4/documentos/teste.txt', 'Nova linha de texto.')
+print(conteudo) 
+
